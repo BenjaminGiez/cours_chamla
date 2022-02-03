71521fe47b7c9d9ff0eb5df59f65b5e76351d2f6
BenjaminGiez <benjamin.giez@outlook.fr>
2022-02-03 10:47:04 +0100

    finished example

--- cours_chamla/introduction.php
+++ cours_chamla/introduction.php
@@ -1,24 +1,40 @@
 <?php
 
 class Employe{
-
     public $nom;
     public $prenom;
     public $age;
 
-    function presentation($prenom, $nom, $age){
-        var_dump("Bonjour, je suis $prenom $nom et j'ai $age ans");
+    public function __construct($prenom, $nom, $age)
+    {
+        $this->prenom = $prenom;
+        $this->nom = $nom;
+        $this->age = $age;
+        var_dump("Je suis construit");
+    }
+
+    public function presentation()
+    {
+        var_dump("Bonjour, je suis $this->prenom $this->nom et j'ai $this->age ans");
     }
 }
 
-$employe1 = new Employe();
+$employe1 = new Employe('Joseph', 'Darymacie', 35);
+//=============================================================
+/*▼ Nous n'avons plus besoin de ces trois lignes ▼
 $employe1->prenom = "Joseph";
 $employe1->nom = "Darymacie";
-$employe1->age= 35;
+$employe1->age= 35;*/
+//Les proprietés se retrouvent entre les parenthèses au dessus ▲
+//=============================================================
 
-$employe2 = new Employe();
-$employe2->prenom = "Uther";
+$employe2 = new Employe('Uther', 'Pendragon', 48); 
+//=============================================================
+/*$employe2->prenom = "Uther";
 $employe2->nom = "Pendragon";
-$employe2->age = 48;
+$employe2->age = 48;*/
+//Même chose que pour $employe1 *
+//=============================================================
 
-$employe1->presentation($employe1->prenom, $employe1->nom, $employe1->age);
+
+$employe1->presentation();
